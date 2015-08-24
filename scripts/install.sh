a3f23aa331dd699bdaad90665801988d1540ca62
Daniel M. Lofaro <dan@danlofaro.com>
2015-08-24 15:33:18 -0400

    forcing yes

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,25 +3,27 @@ sudo ls >> /dev/null
 REPO='ln14ds20'
 #INSTALL_DIR='/home/lofaro/MDS/'
 sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install autoconf automake libtool autoconf-archive
-sudo apt-get install ia32-libs ia32-libs-dev libc6-dev-i386
-sudo apt-get install ia32-libs 
-sudo apt-get install ia32-libs-dev 
-sudo apt-get install libc6-dev-i386
-sudo apt-get install autoconf automake libtool autoconf-archive help2man man2html
-sudo apt-get install libtool
-sudo apt-get install dkms
-sudo apt-get install screen
+sudo apt-get --force-yes -y install build-essential
+sudo apt-get --force-yes -y install autoconf automake libtool autoconf-archive
+sudo apt-get --force-yes -y install ia32-libs ia32-libs-dev libc6-dev-i386
+sudo apt-get --force-yes -y install ia32-libs 
+sudo apt-get --force-yes -y install ia32-libs-dev 
+sudo apt-get --force-yes -y install libc6-dev-i386
+sudo apt-get --force-yes -y install autoconf automake libtool autoconf-archive help2man man2html
+sudo apt-get --force-yes -y install libtool
+sudo apt-get --force-yes -y install dkms
+sudo apt-get --force-yes -y install screen
+sudo apt-get --force-yes -y install python-dev
 
-sudo echo "deb http://code.golems.org/debian squeeze golems.org" > /etc/apt/sources.list.d/ach-sources.list
+echo 'deb http://code.golems.org/debian squeeze golems.org' | sudo tee --append /etc/apt/sources.list.d/ach-sources.list
 sudo apt-get update
 
-sudo apt-get install libach-dev ach-utils ach-dkms
+sudo apt-get --force-yes -y install libach-dev ach-utils ach-dkms
+#sudo apt-get --force-yes -y install libach*
 sudo dpkg-reconfigure ach-utils
 
 sudo ln -s /usr/local/lib/libach* /usr/lib/
 # Install ACH python bindings
-sudo apt-get install python-pip
+sudo apt-get --force-yes -y install python-pip
 sudo pip install http://code.golems.org/src/ach/py_ach-latest.tar.gz
 
